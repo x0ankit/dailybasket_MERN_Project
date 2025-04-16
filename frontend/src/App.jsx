bbc8a37251ced06903e4cd080533e8ad2db4a597
Ankit Raj <ankit.raj70790@gmail.com>
2025-04-16 12:48:38 +0530

    NavBar Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div class = "">
-      <h1>Hello world!</h1>
+    <div>
+      <Navbar/>
     </div>
   );
 };
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+export const AppContext = createContext();
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(true);
+  const [isSeller, setisSeller] = useState(false);
+  const [showUserLogin, setshowUserLogin] = useState(false);
+  const value = { navigate, user, setUser, setisSeller, isSeller,showUserLogin,setshowUserLogin };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};

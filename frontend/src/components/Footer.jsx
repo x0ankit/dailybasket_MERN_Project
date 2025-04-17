dbd3c917adf6c9c87aef1a50287fb4907c63c4a0
Rajsingh294 <147687098+Rajsingh294@users.noreply.github.com>
2025-04-17 13:47:46 +0530

    all products

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,34 +1,23 @@
-const Footer = () => {
-    const linkSections = [
-        {
-            title: "Quick Links",
-            links: ["Home", "Best Sellers", "Offers & Deals", "Contact Us", "FAQs"]
-        },
-        {
-            title: "Need Help?",
-            links: ["Delivery Information", "Return & Refund Policy", "Payment Methods", "Track your Order", "Contact Us"]
-        },
-        {
-            title: "Follow Us",
-            links: ["Instagram", "Twitter", "Facebook", "YouTube"]
-        }
-    ];
+import { assets, footerLinks } from "../assets/assets";
 
+const Footer = () => {
+    
     return (
-        <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
-                    <img className="w-34 md:w-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                    <p className="max-w-[410px] mt-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
+                    <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
+                    <p className="max-w-[410px] mt-6">
+                        We deliver fresh groceries and snacks straight to your door.Trusted by thousands,we aim to make your shopping experience simple and affordable.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
-                    {linkSections.map((section, index) => (
+                    {footerLinks.map((section, index) => (
                         <div key={index}>
                             <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">{section.title}</h3>
                             <ul className="text-sm space-y-1">
                                 {section.links.map((link, i) => (
                                     <li key={i}>
-                                        <a href="#" className="hover:underline transition">{link}</a>
+                                        <a href={link.url} className="hover:underline transition">{link.text}</a>
                                     </li>
                                 ))}
                             </ul>
@@ -37,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright 2025 © PrebuiltUI All Right Reserved.
+                Copyright {new Date().getFullYear()} © GreatStack.dev All Right Reserved.
             </p>
         </div>
     );

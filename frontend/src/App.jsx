50461c3e7b5b1dceb6a28e0a685c6cb47bf59e08
Rajsingh294 <147687098+Rajsingh294@users.noreply.github.com>
2025-04-20 18:09:50 +0530

    my orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import Login from "./components/Login";
 import ProductCategory from "./pages/productCategory";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
 
 
 const App = () => {
@@ -30,6 +32,8 @@ const App = () => {
           <Route path="/products/:category" element={<ProductCategory/>}/>
           <Route path="/products/:category/:id" element={<ProductDetails/>}/>
           <Route path="/cart" element={<Cart/>}/>
+          <Route path="/add-address" element={<AddAddress/>}/>
+          <Route path="/my-orders" element={<MyOrders/>}/>
 
         </Routes>
       </div>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { useAppContext } from "../context/AppContext";
 import { assets, dummyAddress } from "../assets/assets";
 
 const Cart = () => {
-    const {products,currency,cartItems,removeFromCart,getCartCount,updateCartItem,navigate,getCartAmount}=useAppContext()
+    const {products,currency,cartItems,removeFromCart,getCartCount,updateCartItems,navigate,getCartAmount}=useAppContext()
     const [cartArray,setCartArray]=useState([])
     const [addresses,setAddresses]=useState(dummyAddress)
     const [showAddress, setShowAddress] = useState(false)
@@ -33,7 +33,7 @@ const Cart = () => {
         <div className="flex flex-col md:flex-row mt-16">
             <div className='flex-1 max-w-4xl'>
                 <h1 className="text-3xl font-medium mb-6">
-                    Shopping Cart <span className="text-sm text-indigo-500">{getCartCount()} Items</span>
+                    Shopping Cart <span className="text-sm text-primary">{getCartCount()} Items</span>
                 </h1>
 
                 <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -56,7 +56,8 @@ const Cart = () => {
                                     <p>Weight: <span>{product.weight || "N/A"}</span></p>
                                     <div className='flex items-center'>
                                         <p>Qty:</p>
-                                        <select className='outline-none'>
+                                        <select onChange={ (e) => updateCartItems(product._id,Number(e.target.value))} value={cartItems[product._id]}
+                                        className='outline-none'>
                                             {Array(cartItems[product._id]>9 ? cartItems[product._id]:9).fill('').map((_, index) => (
                                                 <option key={index} value={index + 1}>{index + 1}</option>
                                             ))}
@@ -66,13 +67,13 @@ const Cart = () => {
                             </div>
                         </div>
                         <p className="text-center">{currency}{product.offerPrice * product.quantity}</p>
-                        <button onClick={()=>removeFromCart()} className="cursor-pointer mx-auto">
-                            <img src={assets.refresh_icon} alt="remove" className="inline-block w-6 h-6"/>
+                        <button onClick={()=>removeFromCart(product._id)} className="cursor-pointer mx-auto">
+                            <img src={assets.remove_icon} alt="remove" className="inline-block w-6 h-6"/>
                         </button>
                     </div>)
                 )}
 
-                <button onClick={()=>{navigate("/products");scrollTo(0,0)}} className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
+                <button onClick={()=>{navigate("/products");scrollTo(0,0)}} className="group cursor-pointer flex items-center mt-8 gap-2 text-primary font-medium">
                     <img className="group-hover:-translate-x-1 transition" src={assets.arrow_right_icon_colored} alt="arrow" />
                     Continue Shopping
                 </button>
@@ -87,7 +88,7 @@ const Cart = () => {
                     <p className="text-sm font-medium uppercase">Delivery Address</p>
                     <div className="relative flex justify-between items-start mt-2">
                         <p className="text-gray-500">{selectedAddress?`${selectedAddress.street},${selectedAddress.city},${selectedAddress.state},${selectedAddress.country}`:"No address found"}</p>
-                        <button onClick={() => setShowAddress(!showAddress)} className="text-indigo-500 hover:underline cursor-pointer">
+                        <button onClick={() => setShowAddress(!showAddress)} className="text-primary hover:underline cursor-pointer">
                             Change
                         </button>
                         {showAddress && (
@@ -97,7 +98,7 @@ const Cart = () => {
                                     {address.street},{address.city},{address.state},{address.country}
                                 </p>
                             ))}
-                                <p onClick={() => navigate("/add-address")} className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10">
+                                <p onClick={() => navigate("/add-address")} className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10">
                                     Add address
                                 </p>
                             </div>
@@ -129,7 +130,7 @@ const Cart = () => {
                     </p>
                 </div>
 
-                <button onClick={placeOrder} className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition">
+                <button onClick={placeOrder} className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-primary transition">
                     {paymentOption === "COD" ? "Place Order" :"Proceed to Checkout"}
                 </button>
             </div>
